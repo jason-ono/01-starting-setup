f8ad0de98d6b825318b10ec25f066517b6d0d602
jason-ono <exia1999@gmail.com>
2021-07-01 22:58:14 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  // array of JS objects
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', 
+      title: 'New TV', 
+      amount: 799.49, 
+      date: new Date(2021, 2, 12) }
+  ];
+
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
