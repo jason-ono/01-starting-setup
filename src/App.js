e3f0f13e27db34adb5549cd4259e1202b12d84cc
jason-ono <exia1999@gmail.com>
2021-07-04 16:44:32 -0400

    module 1 done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 function App() {
   // array of JS objects
@@ -18,16 +18,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
+      <Expenses items={expenses} />
     </div>
   );
 }
