dbdd9ddf488dbcaf3dc61bc1e352831673a9d6d9
jason-ono <exia1999@gmail.com>
2021-07-04 17:46:20 -0400

    useState

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,29 @@
+import React, {useState} from "react";
 import "./ExpenseItem.css";
 import Card from "./Card";
 import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
+
+  // so-called a React hook—more on this later
+  // these need to be called INSIDE component functions
+
+  const [title, setTitle] = useState(props.title);
+
+  // function for the button
+  const clickHandler = () => {
+    setTitle("Updated!");
+    console.log(title);
+  };
+
   return (  
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-          <h2>{props.title}</h2>
-          <div className="expense-item__price">{props.amount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
